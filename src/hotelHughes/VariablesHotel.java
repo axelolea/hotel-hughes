25695dcf4ea0fc4f19b6557071eaa77daab373c0
axelolea <axelcedilloolea@gmail.com>
2023-06-12 17:27:10 -0600

    Clase con variables del hotel

---
+++ src/hotelHughes/VariablesHotel.java
@@ -0,0 +1,24 @@
+package hotelHughes;
+
+public class VariablesHotel {
+
+    public static void main(String[] args) {
+        // byte, short, int, long, float, double
+
+        byte numeroDePisos = 10;
+
+        short habitaciones = 180;
+
+        int huespedesAnuales = 56979;
+
+        long huespedesMundiales = 2_782_671;
+
+        float costoHabitacion = 149999.99f;
+
+        double costoSuit = 149999.99d;
+
+        boolean habitacionLibre = true;
+
+    }
+
+}
